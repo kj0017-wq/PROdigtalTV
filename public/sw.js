9a43b5926093b5645df8044b664bfe604f8128b6
kj0017-wq <kj0017-wq@users.noreply.github.com>
2026-06-05 16:05:06 +0200

    Revert "Bump CMS content cache version"
    This reverts commit 96799cf47d3d6807c6112f12c3e6cc6111b8042f.

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "pdt-platform-v457";
-const SHELL = ["/", "/index.html", "/manifest.json", "/assets/icon.svg", "/src/styles/main.css?v=457", "/src/main.js?v=457"];
+const CACHE = "pdt-platform-v456";
+const SHELL = ["/", "/index.html", "/manifest.json", "/assets/icon.svg", "/src/styles/main.css?v=456", "/src/main.js?v=456"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
